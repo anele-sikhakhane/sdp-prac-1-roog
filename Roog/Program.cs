f03f2fbff2e246726583188a63c8710a6297f623
Anele Sikhakhane <G18s6106@ict.ru.ac.za>
2020-02-16 14:33:28 +0200

    added student number

--- Roog/Program.cs
+++ Roog/Program.cs
@@ -7,6 +7,7 @@ namespace Roog
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("g18s6106 only plays mk11 & minecraft");
         }
     }
 }
